0bc71919741cfbe3ce9f957410d36eb07aeb0a9e
Feng <Feng@Leo>
2014-10-06 14:15:16 +0800

    Insert Interval

---
+++ Insert Interval/insert.cpp
@@ -0,0 +1,69 @@
+/*
+ * insert.cpp
+ *
+ * Given a set of non-overlapping intervals, insert a new interval into the
+ * intervals (merge if necessary). You may assume that the intervals were
+ * initially sorted according to their start times.
+ *
+ * Example 1:
+ * Given intervals [1,3],[6,9],
+ * insert and merge [2,5] in as [1,5],[6,9].
+ *
+ * Example 2:
+ * Given [1,2],[3,5],[6,7],[8,10],[12,16],
+ * insert and merge [4,9] in as [1,2],[3,10],[12,16].
+ * This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
+ *
+ *  Created on: 2014-10-6
+ *      Author: Feng
+ */
+
+#include <vector>
+using namespace std;
+
+struct Interval {
+	int start;
+	int end;
+
+	Interval()
+		: start(0), end(0)
+	{}
+
+	Interval(int s, int e)
+		: start(s), end(e)
+	{}
+};
+
+vector<Interval> insert(vector<Interval> &intervals, Interval newInterval)
+{
+	vector<Interval> rets;
+
+	Interval candidate = newInterval;
+	bool pushed = false;
+
+	for (vector<Interval>::size_type i = 0; i < intervals.size(); ++i) {
+
+		if (pushed) {
+			rets.push_back(intervals[i]);
+			continue;
+		}
+		if (candidate.end < intervals[i].start) {
+			rets.push_back(candidate);
+			pushed = true;
+			i--;
+			continue;
+		}
+		if (candidate.start > intervals[i].end) {
+			rets.push_back(intervals[i]);
+		}
+		else {
+			candidate.start = min(intervals[i].start, candidate.start);
+			candidate.end = max(intervals[i].end, candidate.end);
+		}
+	}
+
+	if (!pushed)
+		rets.push_back(candidate);
+
+	return move(rets);
+}
