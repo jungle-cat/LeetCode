9e431fcaebc11dc73486076ca841e7a2dddbf091
Feng <Feng@114.214.167.217>
2014-11-06 14:24:20 +0800

    renumbering Two Sum

--- 0027. Two Sum/sum.cpp
+++ 0027. Two Sum/sum.cpp
@@ -29,7 +29,7 @@ vector<int> twoSum(vector<int>& numbers, int target)
 	vector<int> ret(2);
 	unordered_map<int, int> mp;
 
-	for (int i = 0; i < numbers.size(); ++i) {
+	for (vector<int>::size_type i = 0; i < numbers.size(); ++i) {
 		int t = target - numbers[i];
 		if (mp.count(t)) {
 			ret[0] = mp[t]+1;
